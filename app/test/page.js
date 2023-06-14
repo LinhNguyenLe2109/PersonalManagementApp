47de093901946e109aeeefbf25b3a8e03b4838d2
Anwyll Le <lelinhnguyen2109@gmail.com>
2023-06-14 11:54:59 -0400

    need to understand how mongo goes with nextjs

--- app/test/page.js
+++ app/test/page.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import PageContainer from "@/components/PageContainer";
+import React from "react";
 
-function Test() {
-  return (
-    <div>Test</div>
-  )
+import { connectDatabase, disconnectDatabase, getDatabase } from "@/lib/mongodb";
+
+export async function getStaticProps() {
+  await connectDatabase();
+
+  const db = getDatabase();
+  const collection = db.collection("users");
+
+  const data = await collection.findOne({ userName: "user1" });
+
+  await disconnectDatabase();
+
+  return {
+    props: {
+      data,
+      collection,
+      db
+    },
+  };
 }
 
-export default Test
+function Test({ data, collection, db }) {
+  console.log(db);
+  return (
+    <PageContainer>
+      <h1>Testing</h1>
+      <h2>Value from mongoDB users</h2>
+      <div>{typeof collection}</div>
+    </PageContainer>
+  );
+}
+
+export default Test;
